c8d9fbe1593b16ac04b8a9b317e207b139429080
basel <baselk@uia.no>
2018-02-28 21:06:55 +0100

    test to see if it works

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,4 @@
-## Put comments here that give an overall description of what your
+## Just a test Put comments here that give an overall description of what your
 ## functions do
 
 ## Write a short comment describing this function
